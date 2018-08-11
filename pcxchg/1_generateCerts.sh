6b6678d1ed6a73ed401115db7bd839d69bfe3a25
Muhammad Furqan Baqai <baqai.furqan@gmail.com>
2018-08-11 11:56:35 +0400

    Added by baqai.furqan@gmail.com

--- pcxchg/1_generateCerts.sh
+++ pcxchg/1_generateCerts.sh
@@ -1,15 +1,15 @@
 echo "***************** HLF-Network Config Creation *****************"
 echo "---[Recycling directories and old configurations]"
 # Recycling
-rm -fr channels
-rm -fr orderer
-rm -fr crypto-config
+# rm -fr channels
+# rm -fr orderer
+# rm -fr crypto-config
 # Create Respective directories
-mkdir ./channels
-mkdir ./orderer
+# mkdir ./channels
+# mkdir ./orderer
 echo "+++[Creating Certificates]"
 # Create Cedrtificats
-cryptogen generate --config ./crypto-config.yaml
+# cryptogen generate --config ./crypto-config.yaml
 
 echo "+++[Creating Genesis Block]"
 # Create Genesis Block
@@ -26,6 +26,7 @@ echo "+++[Creating Anchor Peers Confirations]"
 configtxgen -profile AsusChannel -outputAnchorPeersUpdate ./channels/asusanchor.tx -channelID asus -asOrg AsusMSP
 configtxgen -profile DellChannel -outputAnchorPeersUpdate ./channels/dellanchor.tx -channelID dell -asOrg DellMSP
 configtxgen -profile HPChannel -outputAnchorPeersUpdate ./channels/hpanchor.tx -channelID hp -asOrg HPMSP
+
 configtxgen -profile AsusChannel -outputAnchorPeersUpdate ./channels/amazonanchorasus.tx -channelID asus -asOrg AmazonMSP
 configtxgen -profile DellChannel -outputAnchorPeersUpdate ./channels/amazonanchordell.tx -channelID dell -asOrg AmazonMSP
 configtxgen -profile HPChannel -outputAnchorPeersUpdate ./channels/amazonanchorhp.tx -channelID hp -asOrg AmazonMSP
